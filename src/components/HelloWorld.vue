e6b19443ee47b2e2558fbc2c271eb484d852cb2d
Muhammadaliabdulmalik <alimalik1403@gmail.com>
2021-12-16 14:52:33 +0700

    membahkan responsive mobile

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -39,37 +39,38 @@
 
     <div class="jumbotron jumbotron-fluid">
       <div class="container tex-center">
-        <img src="../assets/1.png" width="300" class="rounded-circle" />
+        <img src="../assets/1.png" width="20%" class="rounded-circle" />
         <h1 class="display-4">SELAMAT DATANG</h1>
         <p class="lead">SISWA SISWI SMKN15 BANDUNG</p>
       </div>
     </div>
-
-    <div class="container">
-      <div class="row">
-        <div class="col text-center">
-          <h1>about</h1>
+    <section id="about" class="about">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            <h1>about</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <p>
+              Use our powerful mobile-first flexbox grid to build layouts of all
+              shapes and sizes thanks to a twelve column system, five default
+              responsive tiers, Sass variables and mixins, and dozens of
+              predefined classes.
+            </p>
+          </div>
+          <div class="col">
+            <p>
+              Use our powerful mobile-first flexbox grid to build layouts of all
+              shapes and sizes thanks to a twelve column system, five default
+              responsive tiers, Sass variables and mixins, and dozens of
+              predefined classes.
+            </p>
+          </div>
         </div>
       </div>
-      <div class="row">
-        <div class="col">
-          <p>
-            Use our powerful mobile-first flexbox grid to build layouts of all
-            shapes and sizes thanks to a twelve column system, five default
-            responsive tiers, Sass variables and mixins, and dozens of
-            predefined classes.
-          </p>
-        </div>
-        <div class="col">
-          <p>
-            Use our powerful mobile-first flexbox grid to build layouts of all
-            shapes and sizes thanks to a twelve column system, five default
-            responsive tiers, Sass variables and mixins, and dozens of
-            predefined classes.
-          </p>
-        </div>
-      </div>
-    </div>
+    </section>
     <div class="container">
       <div class="row">
         <div class="col">
@@ -171,6 +172,76 @@
         </div>
       </div>
     </div>
+    <section id="contac" class="contac">
+      <div class="container">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center">
+            <h1>contac us</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="card text-white bg-primary mb-3 text-center">
+              <div class="card-body">
+                <h5 class="card-title">contac us</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+            <ul class="list-group">
+              <li class="list-group-item">Cras justo odio</li>
+              <li class="list-group-item">Dapibus ac facilisis in</li>
+              <li class="list-group-item">Morbi leo risus</li>
+              <li class="list-group-item">Porta ac consectetur ac</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+          </div>
+          <div class="col-lg-6">
+            <form>
+              <div class="form-group">
+                <label for="nama">nama</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  width="100%"
+                  id="nama"
+                  placeholder="nama"
+                />
+              </div>
+              <div class="form-group">
+                <label for="email">email</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="email"
+                  placeholder="email"
+                />
+              </div>
+              <div class="form-group">
+                <label for="no telp">no telp</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="no telp"
+                  placeholder="no telp"
+                />
+              </div>
+              <div class="form-group">
+                <label for="pesan">pesan</label>
+                <textarea type="text" class="form-control"></textarea>
+              </div>
+              <div class="form-group">
+                <button type="button" class="btn btn-primary">
+                  kirim pesan
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 <script>
@@ -181,3 +252,4 @@ export default {
   },
 };
 </script>
+<style scoped></style>
