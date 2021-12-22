0d9f5338fc7aaac3234fa27ebcbf89ea8db34361
Muhammadaliabdulmalik <alimalik1403@gmail.com>
2021-12-22 15:50:24 +0700

    final prjock compani profile

--- src/components/A.vue
+++ src/components/A.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <a class="navbar-brand" href="#">tugas 1</a>
       <button
         class="navbar-toggler"
@@ -17,16 +17,16 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="#Home">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">about</a>
+            <a class="nav-link" href="#about">about</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">galeri</a>
+            <a class="nav-link" href="#galeri">galeri</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">content</a>
+            <a class="nav-link" href="#content">content</a>
           </li>
         </ul>
       </div>
@@ -37,41 +37,45 @@
       <div class="container">
         <div class="row">
           <div class="col text-center">
-            <h1 class="">COMPANY PROFILE</h1>
-          </div>
-        </div>
-        <div class="about">
-          <div class="row box-about mb-5 pt-5">
-            <div class="col box-about-col">
-              <h1 class="a">ABOUT</h1>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md box-about-col">
-              <img src="../assets/image-about.jpg" alt="" />
-            </div>
-            <div class="col-md box-about d-flex align-items-center">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi optio voluptates necessitatibus, tempora accusamus
-                molestiae nobis natus, ut similique reiciendis asperiores id
-                modi soluta! Itaque vel libero doloremque aut aspernatur
-                corporis aperiam consectetur quidem, fugiat deleniti quasi alias
-                distinctio, facere minus? Fugiat quidem, qui voluptatibus quam
-                voluptates expedita possimus eos aspernatur tempora sit atque
-                placeat nulla cumque veritatis repellendus dolor recusandae
-                quas, perferendis praesentium deleniti sapiente consequatur
-                ratione et rerum! Velit eum pariatur ex at qui animi molestiae
-                commodi beatae?
-              </p>
+            <div class="col d-flex justify-content-center align-items-center">
+              <h1 class="display-4">COMPANY PROFILE</h1>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <div class="about">
+      <div class="comtainer">
+        <div class="row box-about mb-5 pt-5">
+          <div class="col box-about-col">
+            <h1 class="a">ABOUT</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md box-about-col">
+            <img src="../assets/image-about.jpg" alt="" />
+          </div>
+          <div class="col-md box-about d-flex align-items-center">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+              optio voluptates necessitatibus, tempora accusamus molestiae nobis
+              natus, ut similique reiciendis asperiores id modi soluta! Itaque
+              vel libero doloremque aut aspernatur corporis aperiam consectetur
+              quidem, fugiat deleniti quasi alias distinctio, facere minus?
+              Fugiat quidem, qui voluptatibus quam voluptates expedita possimus
+              eos aspernatur tempora sit atque placeat nulla cumque veritatis
+              repellendus dolor recusandae quas, perferendis praesentium
+              deleniti sapiente consequatur ratione et rerum! Velit eum pariatur
+              ex at qui animi molestiae commodi beatae?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <!-- akhir jumboteron -->
     <section id="galeri" class="galeri">
-      <div class="container">
+      <div class="container box">
         <div class="row mb-5 pt-5">
           <div class="col-md text-center">
             <h1 class="a">galeri</h1>
@@ -141,120 +145,203 @@
             </div>
           </div>
           <div class="col-md">
-            <img
-              class="card-img-top"
-              src="../assets/kari-shea-1SAnrIxw5OY-unsplash.jpg"
-              alt="0"
-            />
-            <div class="card-body">
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-          <div class="col-md">
-            <img
-              class="card-img-top"
-              src="../assets/pedro-costa-aXY5doQNZTc-unsplash.jpg"
-              alt="Card image cap"
-            />
-            <div class="card-body">
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="row mb-5 pt-5">
-          <div class="col-md">
-            <div class="col text-center">
-              <h1 class="a">testimoni</h1>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md">
-            <img
-              class="card-img-top rounded-circle"
-              src="../assets/testi1.jpg"
-              alt="Card-image-to"
-            />
-            <div class="card-body">
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-          <div class="col-md">
-            <img
-              class="card-img-top rounded-circle"
-              src="../assets/testi2.jpg"
-              alt="Card-image-to"
-            />
-            <div class="card-body">
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-          <div class="col-md">
-            <img
-              class="card-img-top rounded-circle"
-              src="../assets/testi3.jpg"
-              alt="Card image cap"
-            />
-            <div class="card-body">
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="row mb-0 pt-4">
-          <div class="col-md">
-            <div class="col text-center">
-              <h1 class="a">Contact Us</h1>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md md-6 mx-auto mt-5">
-            <form action="">
-              <div class="form grup">
-                <label form="nama">useranme</label>
-                <input type="text" class="form-control" id="name" />
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="../assets/kari-shea-1SAnrIxw5OY-unsplash.jpg"
+                alt="0"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
               </div>
-              <div class="form grup">
-                <label form="Email">Email address</label>
-                <input
-                  type="Email"
-                  class="form-control"
-                  id="Email"
-                  aria-describedby="emailHelp"
-                />
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="../assets/pedro-costa-aXY5doQNZTc-unsplash.jpg"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
               </div>
-              <div class="form grup">
-                <label form="Password">Password</label>
-                <input type="Password" class="form-control" id="Password" />
-              </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+            </div>
           </div>
         </div>
-        <footer>
-          <p class="text-center">© muhammd ali abdul malik2021</p>
-        </footer>
       </div>
     </section>
+
+    <section class="testimoni">
+      <div class="row mb-5 pt-5">
+        <div class="col-md">
+          <div class="col text-center">
+            <h1 class="a">testimoni</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md">
+          <img
+            class="card-img-top rounded-circle"
+            src="../assets/testi1.jpg"
+            alt="Card-image-to"
+          />
+          <div class="card-body">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+        <div class="col-md">
+          <img
+            class="card-img-top rounded-circle"
+            src="../assets/testi2.jpg"
+            alt="Card-image-to"
+          />
+          <div class="card-body">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+        <div class="col-md">
+          <img
+            class="card-img-top rounded-circle"
+            src="../assets/testi3.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="contact">
+      <div class="row mb-0 pt-4">
+        <div class="col-md">
+          <div class="col text-center">
+            <h1 class="a" id="content">Contact Us</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <form class="center">
+            <div class="form grup">
+              <label form="nama">useranme</label>
+              <input type="text" class="form-control" id="name" />
+            </div>
+            <div class="form grup">
+              <label form="Email">Email address</label>
+              <input
+                type="Email"
+                class="form-control"
+                id="Email"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div class="form grup">
+              <label form="Password">Password</label>
+              <input type="Password" class="form-control" id="Password" />
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </section>
+    <footer class="bg-dark">
+      <div class="progress">
+        <div
+          class="progress-bar progress-bar-striped"
+          role="progressbar"
+          style="width: 100%"
+          aria-valuenow="10"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        ></div>
+      </div>
+
+      <p class="text-center text-white">© muhammd ali abdul malik2021</p>
+    </footer>
   </div>
 </template>
 <style scoped>
-.about img {
+.jumbotron {
+  padding: 100px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url("../assets/hero-img.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  margin: 0;
+  height: 100vh;
+  background-attachment: fixed;
+}
+.about {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url("../assets/image-about-bg.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  margin: 0;
+  height: 1000px;
+  background-attachment: fixed;
+}
+.galeri {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url("../assets/gallery-bg.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+.galeri .card {
+  background: transparent;
+  color: white;
+}
+
+.testimoni img {
+  width: 125px;
+  height: 125px;
+}
+.contact {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url("../assets/contact-bg.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+.contact form input {
+  background: transparent;
+}
+.folo .about img {
   width: 100%;
+}
+footer p {
+  margin: 0;
+  padding: 15px 0 15px 0;
+}
+.jumbotron h1 {
+  color: rgba(0, 0, 0, 0.5);
+  font-family: "Bebas Neue", cursive;
+  font-size: 90px;
+}
+.navbar-brand {
+  font-family: "Bebas Neue", cursive;
+  font-size: 20px;
 }
 </style>
